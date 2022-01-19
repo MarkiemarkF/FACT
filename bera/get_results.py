e9c3b4c4a2c9f009386327c0e03b41a0e128a47b
flooreijkelboom <eijkelboomfloor@gmail.com>
2022-01-19 14:01:24 +0100

    Original code Bera commit

---
+++ bera/get_results.py
@@ -0,0 +1,35 @@
+import json
+import numpy as np
+import os
+
+def get_res(path="output/2022-01-19-11:01:40"):
+    with open(path, 'r') as f:
+        d = json.load(f)
+
+    E = d['fair_score']
+
+    label_unpr = d['assignment']
+    N = len(d['points'])
+    K = d['num_clusters']
+
+    labels = []
+
+    for i in range(N):
+        for f in range(K):
+            if label_unpr[i * K + f] == 1:
+                labels.append(f)
+
+    return E, labels
+
+
+for dataset in ['student']:
+    path = f'../FACT-main/bera_res/{dataset}.json'
+
+    if not os.path.exists(path):
+        E, l = get_res(f"output/{dataset}")
+        res_dict = {}
+        res_dict['E'] = E
+        res_dict['l'] = l
+
+        with open(path, 'w') as f:
+            json.dump(res_dict, f)
