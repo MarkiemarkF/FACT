01a886021025785480ac38de47b0cf8ebc35c477
MarkiemarkF <mark.fokkema@gmail.com@users.noreply.github.com>
2022-01-26 11:34:10 +0100

    Script that removes the ID column from dataset

--- remove_first_column.py
+++ remove_first_column.py
@@ -1,12 +1,21 @@
 import csv
 
-file = csv.reader(open('data/Synthetic/synthetic.csv'))
+file = csv.reader(open('data/German_Credit/german_credit_data.csv'))
 lines = list(file)
-new_lines = [['A', 'B']]
+# new_lines = [['A', 'B']]
+new_lines = [lines[0]]
 
 for line in lines[1:]:
+    if line[2] == 'male':
+        line[2] = 0
+    elif line[2] == 'female':
+        line[2] = 1
+    else:
+        print('UNKOWN')
+        continue
+
     new_lines.append(line[1:])
 
 print(new_lines)
-writer = csv.writer(open('data/Synthetic/synthetic_mod.csv', 'w', newline=''))
+writer = csv.writer(open('data/German_Credit/german_Credit_mod.csv', 'w', newline=''))
 writer.writerows(new_lines)
