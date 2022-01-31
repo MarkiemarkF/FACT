254b19776da1e448e9633f2f08201c7be63ee5ad
unknown <mark.fokkema@gmail.com>
2022-01-31 12:04:57 +0100

    Added comments to bera/get_results.py

--- bera/get_results.py
+++ bera/get_results.py
@@ -1,8 +1,15 @@
-import json
-import numpy as np
 import os
+import json
 
-def get_res(path="output/2022-01-19-11:01:40"):
+
+def get_res(path=os.path.join('output', '2022-01-19-11:01:40')):
+    """
+    Gets the Energy & labels from a Bera fair clustering output file.
+
+    :param path: path to dataset
+    :return: clustering Energy & labels
+    """
+    # Open Bera output
     with open(path, 'r') as f:
         d = json.load(f)
 
@@ -22,14 +29,16 @@ def get_res(path="output/2022-01-19-11:01:40"):
     return E, labels
 
 
-for dataset in ['student', 'german_credit', 'bank_red']:
-    path = f'../bera_res/{dataset}.json'
+if __name__ == '__main__':
+    # Get results from all files
+    for dataset in ['student', 'german_credit', 'bank_red']:
+        path = os.path.join('..', 'bera_res', dataset + '.json')
 
-    if not os.path.exists(path):
-        E, l = get_res(f"output/{dataset}")
-        res_dict = {}
-        res_dict['E'] = E
-        res_dict['l'] = l
+        if not os.path.exists(path):
+            E, l = get_res(os.path.join('output', dataset))
+            res_dict = {}
+            res_dict['E'] = E
+            res_dict['l'] = l
 
-        with open(path, 'w') as f:
-            json.dump(res_dict, f)
+            with open(path, 'w') as f:
+                json.dump(res_dict, f)
